6c80f2e34389146dc5812dc62a0f5ffa95f628ac
Gil Teixeira <gil@prueslounge.com>
2026-05-20 16:08:01 +0100

    new assets and anims

--- Scripts/powerup.gd
+++ Scripts/powerup.gd
@@ -4,23 +4,23 @@ class_name Powerup
 
 enum Type { FIRE, BOMB }
 
-const _TEXTURES := {
-	Type.FIRE: preload("res://Assets/PowerUpFireUp.png"),
-	Type.BOMB: preload("res://Assets/PowerUpBombUp.png"),
+const _ANIMATIONS := {
+	Type.FIRE: &"fire",
+	Type.BOMB: &"bomb",
 }
 
 const DROPPABLE_TYPES: Array = [Type.FIRE, Type.BOMB]
 
 @export var type: Type = Type.FIRE
 
-@onready var sprite: Sprite2D = $Sprite2D
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 var _collected: bool = false
 
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
-	sprite.texture = _TEXTURES[type]
+	animated_sprite.play(_ANIMATIONS[type])
 
 
 func _on_body_entered(body: Node2D) -> void:
